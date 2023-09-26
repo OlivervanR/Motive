fdef03630db2ee269a39b693e11e2f1556bd8eaf
OlivervanR <140354134+OlivervanR@users.noreply.github.com>
2023-09-25 22:46:52 -0400

    Add files via upload

---
+++ Converter/DefaultBackground.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Test.Converter
+{
+    public class DefaultBackground : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string accountBackground = value as string;
+
+            if (accountBackground == null)
+                return "profilebackgroundwhite";
+            return accountBackground;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Format/BorderlessEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Format
+{
+    public class BorderlessEntry : Entry
+    {
+    }
+}
